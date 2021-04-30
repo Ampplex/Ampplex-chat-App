c9185dae1be3605b821fe3a7848cc2062ef6e379
Ankesh Kumar <ankesh3905222@gmail.com>
2021-04-30 16:49:04 +0530

    Version 8 made Login backened

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-from flask import Flask, render_template, request
+"""
+Copyright (c) Ampplex Technologies Pvt.Ltd. All rights reserved
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import pyttsx3
 import requests
@@ -35,16 +52,36 @@ class Ampplex_UserAuthentication(db.Model):
     host_name = db.Column(db.String(300), nullable=False)
 
     def __repr__(self) -> str:
-        return f"[{self.sno} , {self.user_name} , {self.user_email_id} , {self.country_name} , {self.user_password}, {self.host_name}]"
+        return f"{self.sno} , {self.user_name} , {self.user_email_id} , {self.country_name} , {self.user_password}, {self.host_name}"
 
 
 @app.route('/', methods=['GET', 'POST'])
 def User_Auth():
+
     if request.method == "POST":
-        email_id = request.form['user_email_id']
-        password = request.form['user_password']
-        speak(email_id+password)
-        speak("Hello World")
+        email_id = request.form['user_email_id'].strip()
+        password = request.form['user_password'].strip()
+        USER_DATA = Ampplex_UserAuthentication.query.all()
+
+        # Scaning the data base to ensure that the email-Id and Pass
+        if email_id != "" and password != "":
+            for i in range(len(USER_DATA)):
+                email_id_retrieved = str(USER_DATA[i]).split(',')[2]
+                password_retrieved = str(USER_DATA[i]).split(',')[4]
+                flag_EmailFound = False
+                if email_id_retrieved.strip() == email_id:
+                    flag_EmailFound = True
+                    if password_retrieved.strip() == password:
+                        speak("Successfully Logined")
+                        print("[NEW USER SUCCESSFULLY LOGINED]")
+                        return redirect('/chatroom')
+                    else:
+                        speak("Error: password or email id must be valid and correct")
+
+            if flag_EmailFound == False:
+                speak("Please sign up if you don't have an Ampplex Account")
+        elif email_id == "" and password == "":
+            speak("Please enter the email-Id and password to login")
 
     return render_template('index.html')
 
@@ -96,16 +133,21 @@ def SignUp_Auth():
                 displayMsg = "password length must be atleast 8 characters"
 
                 InnerHtml = Display(type_msg, displayMsg)
-
-            User_Info = Ampplex_UserAuthentication(
-                user_name=name, user_email_id=email_id, user_password=password, country_name=country_name, host_name=host_name)
-            db.session.add(User_Info)
-            db.session.commit()
+            if len(password) >= 8:
+                User_Info = Ampplex_UserAuthentication(
+                    user_name=name, user_email_id=email_id, user_password=password, country_name=country_name, host_name=host_name)
+                db.session.add(User_Info)
+                db.session.commit()
 
         Data = Ampplex_UserAuthentication.query.all()
         print(Data)
 
     return render_template('sign_up.html', InnerHtml=InnerHtml)
+
+
+@app.route('/chatroom')
+def ChatRoom():
+    return render_template('chatroom.html')
 
 
 if __name__ == '__main__':
